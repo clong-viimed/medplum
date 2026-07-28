d176e2fb5c4a43c23a26a9b1d4ef2c98ce157ece
Paul Winterling <pwinterling@hiivehealth.com>
2026-07-28 17:24:31 -0400

    Fix hiive build describe CLI path

--- scripts/deploy-hiive-build-cdk.sh
+++ scripts/deploy-hiive-build-cdk.sh
@@ -42,6 +42,7 @@ case "$action" in
     npx cdk deploy -c config="$config_file" --require-approval never
     ;;
   describe)
+    build_workspace @medplum/cli || return $?
     node packages/cli/dist/cjs/index.cjs aws describe build
     ;;
   *)
